348412667b36e13e397aad15e97dbcc8ae1a9350
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-12-23 22:14:02 -0500

    stateFul components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,122 +1,40 @@
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
 
-const Content = (props) => {
+const Display = (props ) => <div>{props.counter}</div>
 
-    
-    return (
-        <>
-        <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-        <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-        <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-        </>
-    )
-};
+const Button = (props) => (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
 
-const Total = (props) => {
-    return (
-        <div>
-            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
-        </div>
-    )
-}
-
-const Growth = (props) => {
-    return (
-        <p>{props.parts.name}</p>
-    )
-}
-
-//Here this now refers to the referenceToGreet which is a global variable
-//When we bind it to arto, we use arto's internal declared value
-
-const Hello = (props) => {
-
-    const {name, age} = props.parts[4]
-
-    const bornYear = () => new Date().getFullYear() - age
-
-
-    return (
-        <div>
-            <li>
-                Hello {name}, you are {age} years old.
-            </li>
-            <p>
-                So you were probably born in {bornYear()}
-            </p>
-        </div>
-    )
-}
-
-const Counter = (props) => {
-    const {counter} = props
-    return (
-        <div>{counter}</div>
-    )
-}
-
-const App = () => {
-
-    let counter = 1;
-
-    const course = {
-        name: 'Half Stack Web Application',
-        parts : [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },       
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },      
-            {
-                name: 'State of a component',
-                exercises: 14
-            },
-            {
-                name:'Learning React JSX Syntax',
-                exercises: 8
-            },
-            {
-                name:'Shehryar',
-                age: 26
-            }
-        ]
-    }
-
-    const refresh = () => {
-        ReactDOM.render(<App />, document.getElementById('root'));
-    }
-
-    setInterval(() => {
-        refresh()
-        counter += 1
-    }, 5000)
-
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0)
+    const setToValue = (value) => setCounter(value)
 
   return (
     <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-        <Growth parts={course.parts[3]} />
-        <Hello parts={course.parts} />
-        <Counter counter={counter} />
+      <Display counter={counter}/>
+      <Button
+        onClick={() => setToValue(counter + 5)}
+        text='plus'
+      />
+      <Button
+        onClick={() => setToValue(counter * 5)}
+        text='multiply'
+      />
+      <Button
+        onClick={() => setToValue(0)}
+        text='zero'
+      />
     </div>
   )
-};
+}
 
-
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
