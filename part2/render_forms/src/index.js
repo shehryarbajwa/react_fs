ab0d53244f81c2c4f0b93362d9de7153f6524522
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-12-27 19:06:46 -0500

    conditionals added

--- part2/render_forms/src/index.js
+++ part2/render_forms/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import Note from "./Components/Note";
 import Course from "./Components/Course";
@@ -43,9 +43,37 @@ const courses = [
   ];
 
 const App = (props) => {
-    const {notes, courses} = props;
+
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState('a new note...')
+    const [showAll, setShowAll] = useState(true)
+
+
+    const {courses} = props;
   //Here the child component gets the key declared so there is no need to declared it again within the Component
   const rows = () => notes.map(note => <Note key={note.id} note={note} />);
+
+
+  const addNote = (event) => {
+    event.preventDefault();
+    
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1
+    };
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target)
+    setNewNote(event.target.value)
+  }
+
+
 
   const course_rows = () =>
     courses.map(course => (
@@ -64,11 +92,14 @@ const App = (props) => {
   return (
     <div>
       <h1>Half Stack application Development</h1>
-      <h2>Testing hypothesis</h2>
       <ul>{course_rows()}</ul>
       <p>Total: {res()}</p>
       <h1>Notes</h1>
       <ul>{rows()}</ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
