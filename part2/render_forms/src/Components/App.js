742f3c964abae317486cb2cffc69a23f3a26c3ec
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-12-29 21:57:21 -0500

    Note.js updated

--- part2/render_forms/src/Components/App.js
+++ part2/render_forms/src/Components/App.js
@@ -7,18 +7,47 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(note => note.id === id)
+    const changedNote = {...note, important: !note.important}
 
-  // ...
+    axios.put(url, changedNote).then(res => {
+      setNotes(notes.map(note => note.id !== id ? note : res.data))
+    })
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const rows = () => notesToShow.map(note => {
+    <Note
+    key={note.id}
+    note={note}
+    toggleImportance={()=> toggleImportanceOf(note.id)} />
+  })
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() > 0.5,
+    }
+  
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('Hello World...')
+      })
+  }
+
+  return (
+    <>
+    <button onClick={addNote}>Add Note</button>
+    <li>{rows()}</li>
+    </>
+  )
 }
 
 export default App;
